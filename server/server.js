95621272cfb415683c696dc42f1b5014f053dd84
michaelfswann <mikeswann@mail.com>
2021-02-10 23:43:40 +0000

    express

---
+++ server/server.js
@@ -0,0 +1,23 @@
+// External Modules
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// App Variables
+const app = express();
+const port = process.env.SERVER_PORT || "8000";
+
+//  App Configuration
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Routes Definitions
+app.get("/", (req, res) => {
+  res.status(200).send("working");
+});
+
+// Server Activation
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
