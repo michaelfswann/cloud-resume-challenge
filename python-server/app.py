82110943e1de949cab867930d05d544e86e91df4
michaelfswann <mikeswann@mail.com>
2021-02-15 23:28:28 +0000

    python server all working with serverless all requests needed now built

---
+++ python-server/app.py
@@ -0,0 +1,105 @@
+import os
+
+
+import boto3
+
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+COUNTERS_TABLE = os.environ['DYNAMODB_TABLE']
+
+client = boto3.client('dynamodb')
+
+
+@app.route("/")
+def hello():
+
+    return "Hello World!"
+
+
+@app.route("/counters/<string:counter_id>")
+def get_counter(counter_id):
+
+    resp = client.get_item(
+
+        TableName=COUNTERS_TABLE,
+
+        Key={
+
+            'counterId': {'S': counter_id}
+
+        }
+
+    )
+
+    item = resp.get('Item')
+
+    if not item:
+
+        return jsonify({'error': 'User does not exist'}), 404
+
+    return jsonify({
+
+        'counterId': item.get('counterId').get('S'),
+
+        'visitors': item.get('visitors').get('N')
+
+    })
+
+
+@app.route("/counters/<string:counter_id>/increment")
+def increment_counter(counter_id):
+
+    resp = client.update_item(
+
+        TableName=COUNTERS_TABLE,
+
+        Key={
+            'counterId': {'S': counter_id}
+        },
+
+        UpdateExpression='SET visitors = visitors + :inc',
+
+        ExpressionAttributeValues={':inc': {'N': '1'}},
+
+        ReturnValues="UPDATED_NEW"
+    )
+
+    return jsonify({'success': 'visit counter icremented'}), 200
+
+
+@app.route("/counters", methods=["POST"])
+def create_counter():
+
+    counter_id = request.json.get('counterId')
+
+    visitors = request.json.get('visitors')
+
+    if not counter_id or not visitors:
+
+        return jsonify({'error': 'Please provide counterId and visitors'}), 400
+
+    resp = client.put_item(
+
+        TableName=COUNTERS_TABLE,
+
+        Item={
+
+            'counterId': {'S': counter_id},
+
+            'visitors': {'N': visitors}
+
+        }
+
+    )
+
+    return jsonify({
+
+        'counterId': counter_id,
+
+        'visitors': visitors
+
+    })
--- server/db/config.js
+++
@@ -1,4 +0,0 @@
-const REGION = process.env.AWS_REGION;
-const APIVERSION = { apiVersion: "2012-08-10" };
-
-module.exports = { REGION, APIVERSION };
